a2ee692ea147cbfafac0e9ffb9b064133d046f97
brosenmoller <rosenmollerben@gmail.com>
2024-03-24 21:11:31 +0100

    Frontpage setup

---
+++ src/components/Main.svelte
@@ -0,0 +1,38 @@
+<main class="flex flex-1 flex-col font-normal text-wrap text-5xl p-4">
+    <section id="frontPage" class="flex justify-center p-20">
+        <div>
+            <div class="mb-20">Hi,</div>
+            <div>
+                I'm <strong>Ben Rosenmöller</strong>, a passionate Game Developer from The Netherlands, who loves to create, design and learn new things.
+            </div>
+            <div class="mt-5 text-xl">
+                Contact me at 
+                <a href="mailto:rosenmollerben@gmail.com" target="_blank">
+                    rosenmollerben@gmail.com
+                </a>
+            </div>
+        </div>
+    
+        <div class="hidden lg:block ml-10">
+            <img class="h-full object-cover rounded-lg" src="/assets/BenRosenmollerPortrait.jpg" alt="">
+        </div>
+    </section>
+</main>
+
+<style>
+a {
+  text-decoration: none;
+  color: cadetblue;
+  background-image: linear-gradient(currentColor, currentColor);
+  background-position: 0% 100%;
+  background-repeat: no-repeat;
+  background-size: 0% 2px;
+  transition: background-size cubic-bezier(0,.5,0,1) 0.3s;
+}
+
+a:hover,
+a:focus {
+  text-decoration: none;
+  background-size: 100% 2px;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
+<script>
+    import Main from '../components/Main.svelte'
+</script>
 
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Main />
