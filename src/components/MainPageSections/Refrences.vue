fdc38d964721431e2d70a8b0b95884c97ba92692
brosenmoller <rosenmollerben@gmail.com>
2024-04-24 13:53:21 +0200

    added reference link

--- src/components/MainPageSections/Refrences.vue
+++ src/components/MainPageSections/Refrences.vue
@@ -15,27 +15,24 @@
                 <h1 class="font-bold text-2xl">Marc Hoogvliet</h1>
                 <span class="font-thin text-lg">Former Ceo of Jilster B.V.</span>
 
-                <p class="">
-                    Ben started at the age of 15 in the customer service department at your Jilster. There, he very quickly became more confident in voice, which is an important part of service. I look back on that period as Jilster's end manager with great pleasure on Ben's development. He is friendly, polite and clear. He is also honest. At the time, he was allowed to praise himself more for the things that were going well. 
+                <p class="mt-2 mb-5">
+                    As an IT developer, Ben was extremely good and reliable. He developed the cockpit at Jilster. This cockpit was essential for improving the processes of the support staff. Because he had worked there himself, he knew perfectly well what was needed. We were very sorry to see him leave Jilster because of his studies in Utrecht, but were of course happy that he was going to make this step. A step towards a great future.
                 </p>
 
-                <p class="mt-5">
-                    As he worked longer at Jilster, he got more responsibility and then started doing IT development. That is close to his best talent, so from then on, you saw his functioning and performance go up by leaps and bounds. Just as well, I'm glad Ben got to experience service functioning for a while. As an IT developer, Ben was extremely good and reliable. He developed the cockpit at Jilster. This cockpit was essential for improving the processes of the support staff. Because he had worked there himself, he knew perfectly well what was needed. We were very sorry to see him leave Jilster because of his studies in Utrecht, but were of course happy that he was going to make this step. A step towards a great future.
-                </p>
+                <a target="_blank" class="fancy-link text-violet-400"
+                    href="https://drive.google.com/file/d/1gIPQWY0u4R2tbPWc93ksyz7yLuwTZ6Of/view?usp=sharing"
+                >
+                    Link to Full Text
+                </a>
             </div>
+
+            
 
             <div class="lg:w-1/2">
                 <h1 class="font-bold text-2xl">David Scheidt</h1>
                 <span class="font-thin text-lg">Senior Software Developer at Humotion GmbH</span>
 
-                <p class="">
-                    Mr. Ben Rosenmöller, born on 08 March 2003, has completed a one-month internship for career
-                    orientation at Humotion GmbH in the period from 16 November 2021 to 14 December 2021.
-                    The internship was supervised by one of our data analysts Cor van Essen and our senior software
-                    developer David Scheidt.
-                </p>
-
-                <p class="mt-5">
+                <p class="mt-2">
                     During his internship, Mr. Rosenmöller has worked on a new web based editor tool to support internal
                     processes become more efficient. Mr. Rosenmöller's field of learning and tasks included the following:
                     <ul class="dashed">
